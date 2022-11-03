081c9965c627ab6d0efe7043c9f06e4dbf98ac84
Neville Albuquerque <albuquer@ualberta.ca>
2022-11-03 15:17:11 -0600

    added bbd, location, hashid and imagelink

--- app/src/main/java/com/example/cmput301f22t13/domainlayer/item/IngredientItem.java
+++ app/src/main/java/com/example/cmput301f22t13/domainlayer/item/IngredientItem.java
@@ -1,6 +1,13 @@
 package com.example.cmput301f22t13.domainlayer.item;
 
 import java.io.Serializable;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class IngredientItem implements Serializable {
     private String name;
@@ -8,6 +15,10 @@ public class IngredientItem implements Serializable {
     private Integer amount;
     private String unit;
     private String category;
+    private GregorianCalendar bbd;
+    private String location;
+    private String imagelink;
+    private String hashid;
 
     /**
      * Constructor that initializes all ingredient fields to default values
@@ -18,7 +29,12 @@ public class IngredientItem implements Serializable {
         this.amount = 0;
         this.unit = "";
         this.category = "";
+        this.bbd = new GregorianCalendar(0, 0, 0);
+        this.location = "";
+        this.imagelink = "";
+        this.hashid = "";
     }
+
 
     /**
      * Constructor that initializes all ingredient fields to values that are passed in as arguments
@@ -27,13 +43,21 @@ public class IngredientItem implements Serializable {
      * @param amount number of units of this ingredient
      * @param unit unit type for the ingredient (eg. kg, lb)
      * @param category the category that this ingredient belongs to
+     * @param bbd the best before date that this ingredient has
+     * @param imagelink the uri string for storing images for the ingredient
+     * @param hashid the hash ID for uniquely identifying ingredients
+     * @param location the location of the ingredient stored
      */
-    public IngredientItem (String name, String description, Integer amount, String unit, String category) {
+    public IngredientItem (String name, String description, Integer amount, String unit, String category, GregorianCalendar bbd, String imagelink, String location, String hashid) {
         this.name = name;
         this.description = description;
         this.amount = amount;
         this.unit = unit;
         this.category = category;
+        this.bbd = bbd;
+        this.imagelink = imagelink;
+        this.location = location;
+        this.hashid = hashid;
     }
 
     /**
@@ -115,5 +139,62 @@ public class IngredientItem implements Serializable {
     public void setCategory(String category) {
         this.category = category;
     }
+    /**
+     * Gets the best before date the ingredient
+     * @return bbd
+     */
+    public GregorianCalendar getBbd() {
+        return bbd;
+    }
+    /**
+     * Sets the best before date of the ingredient it belongs to
+     * @param bbd new bbd
+     */
+    public void setBbd(GregorianCalendar bbd) {
+        this.bbd = bbd;
+    }
+    /**
+     * Gets the location of this ingredient it belongs to
+     * @return location
+     */
+    public String getLocation() {
+        return location;
+    }
+    /**
+     * Sets the location of the ingredient it belongs to
+     * @param location new location
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    /**
+     * Gets the uri image string of the ingredient it belongs to
+     * @return imagelink
+     */
+    public String getImagelink() {
+        return imagelink;
+    }
+    /**
+     * Sets the uri string for the image of the ingredient it belongs to
+     * @param imagelink new imagelink
+     */
+    public void setImagelink(String imagelink) {
+        this.imagelink = imagelink;
+    }
+    /**
+     * Gets the hash ID of this ingredient it belongs to
+     * @return hashid
+     */
+    public String getHashid() {
+        return hashid;
+    }
+    /**
+     * Sets the hashid of the ingredient it belongs to
+     * @param hashid new hashid
+     */
+    public void setHashid(String hashid) {
+        this.hashid = hashid;
+    }
+
 
 }
