7c2a7f033a0eff478082eaccc43a38cad47a7f2e
Samir Raza Windows Laptop <samirraza09@gmail.com>
2022-11-11 21:01:04 -0700

    Connected shopping list fragment to activity and finished the adapter

--- app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.cmput301f22t13.R;
 import com.example.cmput301f22t13.databinding.ActivityShoppingListBinding;
+import com.example.cmput301f22t13.uilayer.ingredientstorage.IngredientStorageMainFragment;
 
 public class ShoppingListActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
@@ -22,6 +24,13 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_shopping_list);
+
+        Bundle bundle = new Bundle();
+        // bundle.putSerializable(IngredientStorageMainFragment.ARG_INGREDIENT_LIST, ingredientDL.ingredientStorage);
+        navController.setGraph(R.navigation.shopping_list_nav_graph, bundle);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
     }
 }
--- app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListAdapter.java
+++ app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListAdapter.java
@@ -1,6 +1,47 @@
 package com.example.cmput301f22t13.uilayer.shoppinglist;
 
-public class ShoppingListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.example.cmput301f22t13.R;
+import com.example.cmput301f22t13.domainlayer.item.CountedIngredient;
+
+import java.util.ArrayList;
+
+public class ShoppingListAdapter extends ArrayAdapter<CountedIngredient> {
+    private ArrayList<CountedIngredient> countedIngredients;
+    private Context context;
+
+    public ShoppingListAdapter(Context context, ArrayList<CountedIngredient> countedIngredients) {
+        super(context, 0, countedIngredients);
+        this.countedIngredients = countedIngredients;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.content_shopping_list_list, parent, false);
+        }
+
+        CountedIngredient item = countedIngredients.get(position);
+
+        TextView ingredientName = view.findViewById(R.id.counted_ingredient_name_textview);
+        TextView ingredientDescription = view.findViewById(R.id.ingredient_count);
+
+        ingredientName.setText(item.getIngredient().getName());
+        ingredientDescription.setText(item.getCount());
+
+        return view;
+    }
 }
