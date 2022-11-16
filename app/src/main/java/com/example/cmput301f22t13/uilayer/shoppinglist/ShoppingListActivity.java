28dbde22aa5a3e9b940fe3e63e32c989ddc0632a
samirraza09 <ssraza@ualberta.ca>
2022-11-15 19:41:53 -0700

    Started ShoppingListDL

--- app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListActivity.java
@@ -1,6 +1,9 @@
 package com.example.cmput301f22t13.uilayer.shoppinglist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,7 +14,11 @@ import com.example.cmput301f22t13.R;
 import com.example.cmput301f22t13.databinding.ActivityShoppingListBinding;
 import com.example.cmput301f22t13.domainlayer.item.CountedIngredient;
 import com.example.cmput301f22t13.domainlayer.item.IngredientItem;
+import com.example.cmput301f22t13.uilayer.ingredientstorage.IngredientStorageActivity;
 import com.example.cmput301f22t13.uilayer.ingredientstorage.IngredientStorageMainFragment;
+import com.example.cmput301f22t13.uilayer.recipestorage.RecipeStorageActivity;
+
+import java.util.ArrayList;
 
 public class ShoppingListActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
@@ -26,13 +33,30 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        IngredientItem item1 = new IngredientItem();
+        item1.setName("Apple");
+        IngredientItem item2 = new IngredientItem();
+        item2.setName("Pear");
+        CountedIngredient countedIngredient1 = new CountedIngredient(item1, 6);
+        CountedIngredient countedIngredient2 = new CountedIngredient(item2, 12);
+
+        ArrayList<CountedIngredient> countedIngredients = new ArrayList<>();
+        countedIngredients.add(countedIngredient1);
+        countedIngredients.add(countedIngredient2);
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_shopping_list);
 
         Bundle bundle = new Bundle();
-        // bundle.putSerializable(IngredientStorageMainFragment.ARG_INGREDIENT_LIST, ingredientDL.ingredientStorage);
+        bundle.putSerializable(ShoppingListMainFragment.ARG_COUNTED_INGREDIENT_LIST, countedIngredients);
         navController.setGraph(R.navigation.shopping_list_nav_graph, bundle);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListMainFragment.java
+++ app/src/main/java/com/example/cmput301f22t13/uilayer/shoppinglist/ShoppingListMainFragment.java
@@ -21,6 +21,8 @@ import java.util.ArrayList;
 public class ShoppingListMainFragment extends Fragment {
     private FragmentShoppingListMainBinding binding;
     private ArrayAdapter<CountedIngredient> countedIngredientListAdapter;
+    public static final String ARG_COUNTED_INGREDIENT_LIST = "ARG_COUNTED_INGREDIENT_LIST";
+
 
     @Override
     public View onCreateView(
