7e1b49a294e8989fce8fff5b471f920249cfd241
Ishaan <63430708+Irehmani@users.noreply.github.com>
2022-11-16 17:00:13 -0700

    Adding splash screen with intent to login screen

---
+++ app/src/main/java/com/example/cmput301f22t13/uilayer/userlogin/Splash.java
@@ -0,0 +1,35 @@
+package com.example.cmput301f22t13.uilayer.userlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Looper;
+import android.widget.ImageView;
+
+import com.example.cmput301f22t13.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        }, 3000);
+
+
+    }
+
+
+}
